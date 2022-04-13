4536e55d6f3c08982c531d5d154b60ba10dce484
张亚飞 <672233580@qq.com>
2022-04-13 14:44:14 +0800

    Initial Commit

---
+++ ElasticHeader0413/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElasticHeader0413
+//
+//  Created by 张亚飞 on 2022/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ElasticHeader0413/ElasticHeader0413App.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticHeader0413App.swift
+//  ElasticHeader0413
+//
+//  Created by 张亚飞 on 2022/4/13.
+//
+
+import SwiftUI
+
+@main
+struct ElasticHeader0413App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
